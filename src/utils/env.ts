51fe96c30cf8e4eb6ab252b9e67884f32e7c203f
Felipe Lima <talktofill@gmail.com>
2020-03-28 16:03:31 +0900

    Adjust env for heroku app

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 
 export const ENVIRONMENT = process.env.NODE_ENV;
-export const MONGODB_URI = process.env.MONGODB_URI_LOCAL;
+export const {MONGODB_URI} = process.env;
 
 export default {
   ENVIRONMENT,
