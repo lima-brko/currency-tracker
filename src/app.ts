d6ec14d592adf5a80ac1d2f916f3b6e6ab280d55
Felipe Lima <talktofill@gmail.com>
2020-03-28 23:31:38 +0900

    CORDS settings fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import Koa from 'koa';
 import Mongoose from 'mongoose';
 import env from './utils/env';
-import cors from '@koa/cors';
 
 Mongoose.connect(env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -9,11 +8,14 @@ import router from './routes';
 
 const app = new Koa();
 
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 /**
  * API available to any domain for tests
  */
-app.use(cors({origin: '*'}));
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(async(ctx) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+});
 
 export default app;
